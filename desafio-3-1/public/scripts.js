22aa0c1794f6e197b1e44a7788836e4d3ef065fe
TakaGui <gui.yukio175@gmail.com>
2020-06-22 22:43:20 -0300

    implement server

---
+++ desafio-3-1/public/scripts.js
@@ -0,0 +1,26 @@
+const modalOverlay = document.querySelector('.modal-overlay');
+const cards = document.querySelectorAll('.card');
+
+for (let card of cards) {
+  card.addEventListener('click', function() {
+    const courseId = card.getAttribute('id');
+    modalOverlay.classList.add('active');
+    modalOverlay.querySelector('iframe').src= `https://rocketseat.com.br/${courseId}`
+  });
+}
+
+document.querySelector('.close-modal').addEventListener('click', function() {
+  modalOverlay.classList.remove('active');
+  modalOverlay.querySelector('iframe').src = '';
+});
+
+modalOverlay.addEventListener('click', function() {
+  if (modalOverlay.classList.contains('maximize')) {
+    modalOverlay.classList.remove('maximize');
+    modalOverlay.querySelector('.maximize-modal').textContent = 'maximize';
+  } else {
+    modalOverlay.classList.add('maximize');
+    modalOverlay.querySelector('.maximize-modal').textContent = 'minimize';
+  }
+});
+
